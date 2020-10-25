dec7e2272d0b0fc17f9d4a24b50ea5b8ece45185
Harbin Ramo <harbinramo@gmail.com>
2020-10-25 13:04:59 -0400

    Updates as of 2020-10-25 13:04PM: 1. Added new View Model: Details_ViewModel. 2. Updated contents in View/Customer files. 3. Updated contents in View/Movie files.

---
+++ Vidly/Models/ViewModels/Details_ViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Vidly.Models.ViewModels
+{
+    public class Details_ViewModel
+    {
+        public Customer Customer { get; set; }
+
+        public Movie Movie { get; set; }
+
+        public List<Movie> Movies { get; set; }
+
+        public List<Customer> Customers { get; set; }
+
+    }
+}
