59def123c1f803c3b1666c46c06b2698a65b42ab
Harbin Ramo <harbinramo@gmail.com>
2020-10-26 19:44:36 -0400

    Update as of 2020-10-26 19:44PM: 1. Updated Movie Controller.

--- Vidly/Controllers/MovieController.cs
+++ Vidly/Controllers/MovieController.cs
@@ -3,14 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Vidly.Models;
+using Vidly.Models.ViewModels;
 
 namespace Vidly.Controllers
 {
     public class MovieController : Controller
     {
-        public IActionResult Index()
+        private Details_ViewModel _details;
+        private ApplicationDbContext _context;
+        
+        public MovieController(ApplicationDbContext ctx)
         {
-            return View();
+            this._context = ctx;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            this._context.Dispose();
+        }
+
+        public ViewResult Index()
+        {
+            ViewBag.Title = "Movies";
+
+            this._details = new Details_ViewModel();
+            this._details.Movies = this._context.Movies
+                .Include(m => m.Genre)
+                .OrderBy(m => m.Id);
+
+            return View(this._details);
         }
     }
 }
