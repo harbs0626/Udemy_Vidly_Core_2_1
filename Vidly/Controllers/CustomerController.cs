54e1159bbca9f8c72927a164bc23ea8825f6a384
Harbin Ramo <harbinramo@gmail.com>
2020-10-26 22:13:14 -0400

    Update as of 2020-10-26 22:13PM: 1. Updated Customer Controller.

--- Vidly/Controllers/CustomerController.cs
+++ Vidly/Controllers/CustomerController.cs
@@ -35,5 +35,56 @@ namespace Vidly.Controllers
 
             return View(this._details);
         }
+
+        public object NewRecord()
+        {
+            ViewBag.Title = "New Customer";
+
+            var _viewModel = new Form_Customer_ViewModel()
+            {
+                Customer = new Customer(),
+                MembershipTypes = this._context.MembershipTypes.ToList()
+            };
+
+            return _viewModel;
+        }
+
+        public ViewResult Record()
+        {
+            return View(this.NewRecord());
+        }
+
+        public ActionResult Save(Details_ViewModel _customer)
+        {
+            if (ModelState.IsValid)
+            {
+                if (_customer.Customer.Id == 0)
+                {
+                    this._context.Customers.Add(_customer.Customer);
+                }
+                else
+                {
+                    var _customerInDb = this._context.Customers
+                        .SingleOrDefault(c => c.Id == _customer.Customer.Id);
+
+                    if (_customerInDb != null)
+                    {
+                        _customerInDb.Name = _customer.Customer.Name;
+                        _customerInDb.BirthDate = _customer.Customer.BirthDate;
+                        _customerInDb.MembershipType.Id = _customer.Customer.MembershipType.Id;
+                        _customerInDb.IsSubscribedToNewsLetter = _customer.Customer.IsSubscribedToNewsLetter;
+                    }
+                }
+
+                this._context.SaveChanges();
+            }
+            else
+            {
+                return View("Form_Customer", this.NewRecord());
+            }
+
+            return RedirectToAction("Index", "Customer");
+        }
+
     }
 }
