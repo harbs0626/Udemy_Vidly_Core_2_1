db2933cbe0460e6ea34d682afdd08819699471ad
Harbin Ramo <harbinramo@gmail.com>
2020-10-28 09:15:24 -0400

    Update as of 2020-10-28 09:13AM: 1. Added Controllers/API: Customer.

---
+++ Vidly/Controllers/API/CustomerController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private ApplicationDbContext _context;
+
+        public CustomerController(ApplicationDbContext ctx)
+        {
+            this._context = ctx;
+        }
+
+        // GET: API/<controller>
+        [HttpGet]
+        public IEnumerable<Customer> GetCustomers()
+        {
+            return this._context.Customers
+                .OrderBy(c => c.Id);
+        }
+
+        // GET: API/<controller>/<id>
+        [HttpGet("{Id}")]
+        public IActionResult GetCustomer(int Id)
+        {
+            var _customer = this._context.Customers
+                .SingleOrDefault(c => c.Id == Id);
+
+            if (_customer == null)
+            {
+                return NotFound("Customer does not exist!");
+            }
+
+            return Ok(_customer);
+        }
+
+        // POST: API/<controller>
+        [HttpPost]
+        public IActionResult CreateCustomer(Customer _customer)
+        {
+            if (ModelState.IsValid)
+            {
+                this._context.Customers.Add(_customer);
+                this._context.SaveChanges();
+            }
+
+            return Ok("Successfully added Customer.");
+        }
+
+        // PUT: API/<controller>/<id>
+        [HttpPut("{Id}")]
+        public void UpdateCustomer(int Id, Customer _customer)
+        {
+            var _customerInDb = this._context.Customers
+                .SingleOrDefault(c => c.Id == Id);
+
+            if (_customerInDb != null)
+            {
+                _customerInDb.Name = _customer.Name;
+                _customerInDb.BirthDate = _customer.BirthDate;
+                _customerInDb.MembershipTypeId = _customer.MembershipTypeId;
+                _customerInDb.IsSubscribedToNewsLetter = _customer.IsSubscribedToNewsLetter;
+            }
+
+            this._context.SaveChanges();
+        }
+
+        // DELETE: API/<controller>/<id>
+        [HttpDelete("{Id}")]
+        public void DeleteCustomer(int Id)
+        {
+            var _customerInDb = this._context.Customers
+                .SingleOrDefault(c => c.Id == Id);
+
+            if (_customerInDb != null)
+            {
+                this._context.Customers.Remove(_customerInDb);
+                this._context.SaveChanges();
+            }
+        }
+
+    }
+}
