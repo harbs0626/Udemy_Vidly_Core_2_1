1bb8f2e447fdbfdf39c2dbb4094b6717ed2b0c65
Harbin Ramo <harbinramo@gmail.com>
2020-11-03 11:37:03 -0500

    Update as of 2020-11-03 11:33AM: 1. Updated Views/Customer: Index to cater Bootbox and DataTable. 2. Updated libman and Views/Shared: _Layout to add Bootbox and DataTable scripts. 3. Added bootbox and datatables in wwwroot/lib. 4. Added simple INSERT SELECT script to populate Customers table.

---
+++ Vidly/_DbScripts/Populate Table using While Loop and InsertSelect.sql
@@ -0,0 +1,17 @@
+﻿-- >> Author: Harbin Ramo
+-- >> Date: 2020-11-03 10:08AM
+
+-- 1. (Optional) Clear any data from <TableName> Table
+--TRUNCATE TABLE <TableName>
+
+-- 2. While Loop and InsertSelect
+DECLARE @MinValue INT
+DECLARE @MaxValue INT
+SET @MinValue = 1
+SET @MaxValue = 100
+WHILE @MinValue <= @MaxValue
+	BEGIN
+		INSERT INTO Customers (Name, BirthDate, IsSubscribedToNewsLetter, MembershipTypeId) SELECT 'Harbin_'+CONVERT(NVARCHAR,@MinValue),'2020-06-26',0,1
+		SET @MinValue = @MinValue + 1
+	END
+	
