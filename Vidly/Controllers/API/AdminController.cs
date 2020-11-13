42a5e91e49f0259ef4876d28bfe13bdbfdb71f1b
Harbin Ramo <harbinramo@gmail.com>
2020-11-13 10:26:05 -0500

    Update as of 2020-11-13 10:24AM: 1. Added Controllers/API: Admin. 2. Added Controlelrs: Admin. 3. Added Views/Admin: UserList. 4. Updated Views/Customer: Index.

---
+++ Vidly/Controllers/API/AdminController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Vidly.Models;
+
+namespace Vidly.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+        private UserManager<AppUser> _userManager;
+        private AppIdentityDbContext _context;
+
+        public AdminController(UserManager<AppUser> _userManagerRepo, 
+            AppIdentityDbContext _appIdentityDbCtx)
+        {
+            this._userManager = _userManagerRepo;
+            this._context = _appIdentityDbCtx;
+        }
+
+        // GET: API/<controller>
+        [HttpGet]
+        public IActionResult GetUsers()
+        {
+            var _getUsers = this._context.AppUsers
+                //.Include(u => u.IdentityRole)
+                .ToList()
+                .OrderBy(u => u.Id);
+
+            return Ok(_getUsers);
+        }
+
+        // DELETE: API/<controller>/<id>
+        [HttpDelete("{Id}")]
+        public async Task<IActionResult> DeleteUser(string Id)
+        {
+            AppUser _deleteUser = await this._userManager
+                .FindByIdAsync(Id.ToString());
+
+            if (_deleteUser == null)
+            {
+                return NotFound("User does not exist!");
+            }
+
+            string _message = "Successfully deleted Customer.";
+            IdentityResult _result = await this._userManager.DeleteAsync(_deleteUser);
+            if (_result.Succeeded)
+            {
+                _message = "Successfully deleted Customer.";
+            }
+            return Ok(_message);
+        }
+
+    }
+}
---
+++ Vidly/Controllers/AdminController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Vidly.Models;
+using Vidly.Models.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class AdminController : Controller
+    {
+        private Form_User_ViewModel _users;
+        private AppIdentityDbContext _context;
+
+        public AdminController(AppIdentityDbContext _ctx)
+        {
+            this._context = _ctx;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            this._context.Dispose();
+        }
+
+        public ViewResult UserList()
+        {
+            ViewBag.Title = "List of Users";
+
+            this._users = new Form_User_ViewModel();
+            this._users.AppUsers = this._context.AppUsers
+                .Where(u => u.UserType != null)
+                .OrderBy(u => u.Id);
+            this._users.AppRoles = this._context.Roles
+                .Where(r => r.Name != "Admin").ToList();
+
+            return View(this._users);
+        }
+
+    }
+}
